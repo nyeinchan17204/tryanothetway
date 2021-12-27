f2bf830a8c81fa100381bd6aba537b28e097d268
nyeinchan17204 <92505220+nyeinchan17204@users.noreply.github.com>
2021-12-27 11:00:44 +0630

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,5 @@
 class config:
-    BOT_TOKEN = "5015365351:AAHZ6QM_TFe_FADrLcZ3o7VZoIY4xPFq8YU"
+    BOT_TOKEN = "5015365351:AAEZQAliYFw-ISEL11uvdrpcYrF-C3qZmEM"
     APP_ID = "7693500"
     API_HASH = "8d82e2ae3917b001afb9a3e2c1ba2ce6"
     DATABASE_URL = "postgresql://postgres:jJnOpzd28jOS6GU3M8a1@containers-us-west-23.railway.app:7702/railway"
